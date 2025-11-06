09001f486da4bc9440c4620dd0f30dda78fb8f15
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-06 11:48:24 +0500

    Task1

---
+++ Tyuiu.ZolotovaKA.Sprint2.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.ZolotovaKA.Sprint2.Task1.V18.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint2.Task1.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 155;
+            int b = 696;
+            int c = 242;
+            int d = 541;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, true, true, false, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.ZolotovaKA.Sprint2.Task1.V18/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.ZolotovaKA.Sprint2.Task1.V18.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int a = 155;
+        int b = 696;
+        int c = 242;
+        int d = 541;
+
+
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+
+
+
+
+        Console.Title = "Спринт #2 | Выполнила: Золотова К. А.  | ИСПб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнила:  Золотова К. А.  | ИСПб-24-1                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*Написать программу из операций сравнений *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("a = " + a);
+
+        Console.WriteLine("b = " + b);
+
+        Console.WriteLine("c = " + c);
+
+        Console.WriteLine("d = " + d);
+
+
+
+
+
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("* Результат:                                                                   *");
+        Console.WriteLine("********************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+
+        Console.ReadKey();
+
+    }
+}
